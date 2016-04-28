5ff302a1c05ecd177c9c1097e667c71b705bfb0d
Kh40sz3r0 <sergiomr1988@hotmail.com>
2016-04-28 00:59:55 -0500

    correcting deployment

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Deploying to $TRAVIS_BRANCH"
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    firebase deploy --token "$FIREBASE_TOKEN_PROD"
-elif [ "$TRAVIS_BRANCH" == "develop" ]; then
-    firebase deploy --token "$FIREBASE_TOKEN_DEVELOP"
-fi
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "setup $TRAVIS_BRANCH"
-if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
-    npm install -g firebase-tools
-fi
