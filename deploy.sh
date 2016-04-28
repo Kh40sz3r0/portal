999058c9e358dead508be0a91310e53b1eb0ef53
Kh40sz3r0 <sergiomr1988@hotmail.com>
2016-04-27 23:29:06 -0500

    adding scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
+echo "Deploying to $TRAVIS_BRANCH"
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    echo "Deploying to prod"
     firebase deploy --token "$FIREBASE_TOKEN_PROD"
 elif [ "$TRAVIS_BRANCH" == "develop" ]; then
-    echo "Deploying to develop"
     firebase deploy --token "$FIREBASE_TOKEN_DEVELOP"
 fi
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
+echo "setup $TRAVIS_BRANCH"
 if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
-    echo "setup $TRAVIS_BRANCH"
     npm install -g firebase-tools
 fi
