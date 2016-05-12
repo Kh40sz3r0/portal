7f41b9cb21435ed732b35392b819576ba92b861c
Kh40sz3r0 <sergiomr1988@hotmail.com>
2016-05-12 01:36:20 -0400

    removing un used gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -224,7 +224,7 @@ gulp.task('build-specs', ['templatecache'], function (done) {
  * This is separate so we can run tests on
  * optimize before handling image or fonts
  */
-gulp.task('build', ['optimize', 'images', 'fonts', 'translations'], function () {
+gulp.task('build', ['optimize', 'images', 'fonts'], function () {
   log('Building everything');
 
   var msg = {
